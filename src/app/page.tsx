17162d2f3300c0b9b83ba71f4d08cf6e4e98d507
Andres Rodriguez <ancaro1487@gmail.com>
2024-09-02 13:31:50 -0500

    LVBF-57: first version of orchestra navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { Navbar, SearchFlightsBanner, TopBar } from '@/components'
+import { Navbar, OldNavbar, SearchFlightsBanner, TopBar } from '@/components'
 
 export default function Home() {
   return (
     <>
-      <TopBar height={60}>
+      <TopBar height={106}>
         <Navbar />
+        <OldNavbar />
       </TopBar>
       <SearchFlightsBanner />
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,2 +1,3 @@
 export * from './Navbar'
 export * from './CommonLinks'
+export * from './OldNavbar'
