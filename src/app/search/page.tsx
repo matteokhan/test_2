ff3aefe1aaf2b735b2140618bf6c36bfadb23de9
davidsv <d.soeiro-vuong@tw3partners.com>
2025-03-13 08:53:22 +0100

    header out

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -406,24 +406,6 @@ export default function FlighsPage() {
 
   return (
     <>
-      <TopBar height={isDesktop ? 120 : 230} fixed={isDesktop ? false : true}>
-        <Navbar />
-        <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
-          <OldNavbar />
-        </Box>
-        <Box sx={{ display: { xs: 'block', lg: 'none' } }}>
-          <SelectedFlightInfoTopbarMobile
-            withFilters
-            withAgencySelector
-            onOpenFilters={(filterName) => {
-              setActiveFilter(filterName)
-              setActiveFilterOpen(true)
-            }}
-            onSearch={searchFlights}
-            isLoading={isLoading}
-          />
-        </Box>
-      </TopBar>
       <Box
         sx={{
           backgroundColor: 'grey.200',
